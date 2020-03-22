4b42a02ec80460776196fd97e48f729895de5302
Chris Enitan <enitanchris@gmail.com>
2020-03-22 17:41:37 +0100

    add post and fallcback for empty post

--- nodepress/routes/sqlRoute.js
+++ nodepress/routes/sqlRoute.js
@@ -29,7 +29,7 @@ approuter.get('/createdb', (req, res) => {
     let sql = 'CREATE DATABASE node';
     sqldb.query(sql, (err, result) => {
         if(err) throw err;
-        console.log(result);
+        console.log(`${result} Created node db Done...`);
         res.status(200).json({
 			message: 'Database created...',
 			status: "passed"
@@ -42,12 +42,57 @@ approuter.get('/dropdb', (req, res) => {
     let sql = 'DROP DATABASE IF EXISTS node;';
     sqldb.query(sql, (err, result) => {
         if(err) throw err;
-        console.log(result);
+        console.log(`${result} Dropped node db Done...`);
         res.send('Database dropped...');
     });
 });
 
+//create a table
+approuter.get("/createpostable", (req, res) => {
+	let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), owner VARCHAR(255), PRIMARY KEY(id))';
+	sqldb.query(sql, (err, result) =>{
+		if(err) throw err
+		console.log(`${result} Created post tb Done...`)
+		res.status(200).json({
+			message: 'Table posts created...',
+			status: "passed"
+		})
+	})
+})
 
 
+//insert a data
+approuter.get("/addpost", (req, res) =>{
+	//create a post. could also be gotten from req
+	var postData = req.body;
+	//make sure somthing was posted
+ 	if(!req.body.title){
+		var postData = {
+		title: "Faster Higher Farther",
+		body: "Book on the history of auto industry and it's corrupt stories",
+		owner: "Jack Ewing"
+	}}
+	
+	let sql = 'INSERT INTO posts SET ?';
+	sqldb.query(sql, postData, (err, result) =>{
+		if(err) throw err
+		console.log(`${result} new post added...`)
+		res.status(200).json({
+			message: "New post added",
+			status: "Passed"
+		})
+	})
+})
+
+
+// Select posts
+approuter.get('/getallposts', (req, res) => {
+    let sql = 'SELECT * FROM posts';
+    let query = sqldb.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send(results);
+    });
+});
 
 module.exports = approuter;
