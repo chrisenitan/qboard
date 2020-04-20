aa366eaebab6c66f8794b3594ba39d3ac9c42f6c
Chris Enitan <enitanchris@gmail.com>
2020-04-20 20:14:33 +0200

    db acces from fhs

--- nodepress/routes/sqlRoute.js
+++ nodepress/routes/sqlRoute.js
@@ -5,18 +5,9 @@ const mysql = require('mysql')
 //initislaize express
 const approuter = express();
 
+
 /* 
-FREE SQL SERVER:
-Server: sql2.freemysqlhosting.net
-Name: sql2333143
-Username: sql2333143
-Password: hA8!cB3%
-Port number: 3306
-IP: 54.247.107.148
-
-// Create connection. this is now part of index js. to be tested when sever is online
-
-const pause = mysql.createConnection({
+const sqldb = mysql.createConnection({
     host     : process.env.SQLServer,
     user     : 'admin_chris',
     password : 'staging123',
@@ -25,9 +16,9 @@ const pause = mysql.createConnection({
 */
 
 const sqldb = mysql.createConnection({
-    host     : '54.247.107.148',
+    host     : process.env.freeHostingServer,
     user     : 'sql2333143',
-    password : 'hA8!cB3%',
+    password : process.env.fHSPassword,
 	database : 'sql2333143'
 });
 
