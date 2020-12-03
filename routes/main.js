b7ec205f204c57649b606d5dea615abea6e23824
chrisenitan <enitanchris@gmail.com>
2020-12-03 10:25:14 +0100

    profile main router updates, support own and public proifiles

--- routes/main.js
+++ routes/main.js
@@ -73,13 +73,15 @@ approuter.get('/signup', (req, res) => {
 
 //LOAD PROFILE DEFAULT FOR ALL ROOT LINKS EXPECT DEFINED
 approuter.get('/:username', (req, res) => {
-    //chedk for login or url
-    
-    let userUsername = req.params.username
-    let userCookie = req.cookies.user
-    console.log(userCookie + " cookie: " + userUsername)
+    //check if own profile or not
 
-    let checkForUser = `SELECT * FROM profiles WHERE username =` + sqldb.escape(userUsername) + `AND cookie = ` + sqldb.escape(userCookie);
+    let userUsername = req.params.username
+
+    if(req.cookies.user != undefined){
+        //login cookie user
+        let userCookie = req.cookies.user
+        console.log(userCookie + " cookie: " + userUsername)
+        let checkForUser = `SELECT * FROM profiles WHERE username =` + sqldb.escape(userUsername) + `AND cookie = ` + sqldb.escape(userCookie);
     sqldb.query(checkForUser, (err, result)=>{
         if (err) throw err;
         
@@ -97,6 +99,33 @@ approuter.get('/:username', (req, res) => {
            res.render("profile", foundUser)
         }
     }) 
+    }
+    //not a cookie user, must be public page visit?
+    else{
+        let checkForUser = `SELECT * FROM profiles WHERE username =` + sqldb.escape(userUsername)
+    sqldb.query(checkForUser, (err, result)=>{
+        if (err) throw err;
+        
+        //all these are already done on main route. we should just collect the object or check if its a login or url vivist
+        if(Object.keys(result).length == 0){
+            console.log("User not found.")
+            res.status(204).json({
+                message:"no user found"
+            })
+        }
+        //user found
+        else{
+           console.log(result)
+           let foundUser = Object.assign(result[0], ["name","username","","hint","","","image",""])
+           res.render("profile", foundUser)
+        }
+    }) 
+    }
+    
+    
+   
+
+    
 
 });
 
